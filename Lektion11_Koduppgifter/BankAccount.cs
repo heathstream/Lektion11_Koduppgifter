310cfff18474407142f18346d58d5e0487bd5ac0
Anders Hedström <toastwaffle@gmail.com>
2025-11-02 10:54:12 +0100

    Added BankAccount class

---
+++ Lektion11_Koduppgifter/BankAccount.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lektion11_Koduppgifter
+{
+    public class BankAccount
+    {
+        public decimal Balance { get; private set; }
+        public int AccountNumber { get; }
+    }
+}
