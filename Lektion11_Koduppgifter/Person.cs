177fdcdefa57b94c5272060cab4dd08c64179ac8
Anders Hedström <toastwaffle@gmail.com>
2025-11-02 10:36:44 +0100

    split Person class into own file

---
+++ Lektion11_Koduppgifter/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lektion11_Koduppgifter
+{
+    public class Person
+    {
+        string Name { get; set; }
+        int Age { get; set; }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public void Introduce()
+        {
+            Console.WriteLine($"Hello! My name is {Name} and I'm {Age} years old.");
+        }
+    }
+}
--- Lektion11_Koduppgifter/Program.cs
+++ Lektion11_Koduppgifter/Program.cs
@@ -12,21 +12,4 @@
             person.Introduce();
         }
     }
-
-    public class Person
-    {
-        string Name { get; set; }
-        int Age { get; set; }
-
-        public Person(string name, int age)
-        {
-            Name = name;
-            Age = age;
-        }
-
-        public void Introduce()
-        {
-            Console.WriteLine($"Hello! My name is {Name} and I'm {Age} years old.");
-        }
-    }
 }
