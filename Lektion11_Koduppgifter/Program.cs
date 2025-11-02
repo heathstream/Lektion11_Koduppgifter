093dab2d5c75f221aab26121193ce99184dde421
Anders Hedström <toastwaffle@gmail.com>
2025-11-02 10:15:43 +0100

    Added constructor to Person class

--- Lektion11_Koduppgifter/Program.cs
+++ Lektion11_Koduppgifter/Program.cs
@@ -14,5 +14,11 @@
     {
         string Name { get; set; }
         int Age { get; set; }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
     }
 }
