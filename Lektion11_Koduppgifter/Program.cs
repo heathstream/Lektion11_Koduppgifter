5367072d2dbf3f08a3edc7d63a2538ff7ad42df2
Anders Hedström <toastwaffle@gmail.com>
2025-11-02 10:05:09 +0100

    Add project files.

---
+++ Lektion11_Koduppgifter/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lektion11_Koduppgifter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Git!");
+        }
+    }
+}
