d5380f9e30d9266a4df7f235f323ea3de78a7324
Anders Hedström <toastwaffle@gmail.com>
2025-11-02 10:22:06 +0100

    Added Subtract method to Calculator class

---
+++ Lektion11_Koduppgifter/Calculator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lektion11_Koduppgifter
+{
+    public class Calculator
+    {
+        public int Subtract(int a, int b) => a - b;
+    }
+}
