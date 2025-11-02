9cbb6cf593378af2b7a164e562e330a3b2c70c94
Anders Hedström <toastwaffle@gmail.com>
2025-11-02 10:16:55 +0100

    Added Introduce method to Person class

--- Lektion11_Koduppgifter/Program.cs
+++ Lektion11_Koduppgifter/Program.cs
@@ -20,5 +20,10 @@
             Name = name;
             Age = age;
         }
+
+        public void Introduce()
+        {
+            Console.WriteLine($"Hello! My name is {Name} and I'm {Age} years old.");
+        }
     }
 }
